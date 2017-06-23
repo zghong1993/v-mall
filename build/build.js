3913faac50a66df5149b10a5d57a95036dde9205
zhaoguohong <zhaoguohong@terminus.io>
2017-06-23 16:11:41 +0800

    bug 修复

--- build/build.js
+++ build/build.js
@@ -14,24 +14,24 @@ var spinner = ora('building for production...')
 spinner.start()
   //删除已编译文件
 rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
-  if (err) throw err /
+  if (err) throw err
     //在删除完成的回调函数中开始编译
-    webpack(webpackConfig, function(err, stats) {
-      spinner.stop()
-      if (err) throw err
-        // 在编译完成的回调函数中,在终端输出编译的文件
-      process.stdout.write(stats.toString({
-        colors: true,
-        modules: false,
-        children: false,
-        chunks: false,
-        chunkModules: false
-      }) + '\n\n')
+  webpack(webpackConfig, function(err, stats) {
+    spinner.stop()
+    if (err) throw err
+      // 在编译完成的回调函数中,在终端输出编译的文件
+    process.stdout.write(stats.toString({
+      colors: true,
+      modules: false,
+      children: false,
+      chunks: false,
+      chunkModules: false
+    }) + '\n\n')
 
-      console.log(chalk.cyan('  Build complete.\n'))
-      console.log(chalk.yellow(
-        '  Tip: built files are meant to be served over an HTTP server.\n' +
-        '  Opening index.html over file:// won\'t work.\n'
-      ))
-    })
+    console.log(chalk.cyan('  Build complete.\n'))
+    console.log(chalk.yellow(
+      '  Tip: built files are meant to be served over an HTTP server.\n' +
+      '  Opening index.html over file:// won\'t work.\n'
+    ))
+  })
 })
