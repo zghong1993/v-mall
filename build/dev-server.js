de858535a7dc15147efca7fe5d716e22f634e264
zhaoguohong <zhaoguohong@terminus.io>
2017-10-14 20:22:50 +0800

    format code

--- build/dev-server.js
+++ build/dev-server.js
@@ -1,50 +1,51 @@
 require('./check-versions')()
-var config = require('../config')
+const config = require('../config')
+
 if (!process.env.NODE_ENV) {
   process.env.NODE_ENV = JSON.parse(config.dev.env.NODE_ENV)
 }
-var opn = require('opn') //强制打开浏览器
-var path = require('path')
-var express = require('express')
-var webpack = require('webpack')
-var proxyMiddleware = require('http-proxy-middleware')
-var webpackConfig = require('./webpack.dev.conf')
+const opn = require('opn') // 强制打开浏览器
+const path = require('path')
+const express = require('express')
+const webpack = require('webpack')
+const proxyMiddleware = require('http-proxy-middleware')
+const webpackConfig = require('./webpack.dev.conf')
 // default port where dev server listens for incoming traffic
-var port = process.env.PORT || config.dev.port
+const port = process.env.PORT || config.dev.port
 // automatically open browser, if not set will be false
-var autoOpenBrowser = !!config.dev.autoOpenBrowser
+const autoOpenBrowser = !!config.dev.autoOpenBrowser
 // Define HTTP proxies to your custom API backend
 // https://github.com/chimurai/http-proxy-middleware
-var proxyTable = config.dev.proxyTable
-var app = express()
-var compiler = webpack(webpackConfig)
-//webpack-dev-middleware的作用
-//1.将编译后的生成的静态文件放在内存中,所以在npm run dev后磁盘上不会生成文件
-//2.当文件改变时,会自动编译。
-//3.当在编译过程中请求某个资源时，webpack-dev-server不会让这个请求失败，而是会一直阻塞它，直到webpack编译完毕
-var devMiddleware = require('webpack-dev-middleware')(compiler, {
+const proxyTable = config.dev.proxyTable
+const app = express()
+const compiler = webpack(webpackConfig)
+// webpack-dev-middleware的作用
+// 1.将编译后的生成的静态文件放在内存中,所以在npm run dev后磁盘上不会生成文件
+// 2.当文件改变时,会自动编译。
+// 3.当在编译过程中请求某个资源时，webpack-dev-server不会让这个请求失败，而是会一直阻塞它，直到webpack编译完毕
+const devMiddleware = require('webpack-dev-middleware')(compiler, {
   publicPath: webpackConfig.output.publicPath,
-  quiet: true
+  quiet: true,
 })
-//webpack-hot-middleware的作用就是实现浏览器的无刷新更新
-var hotMiddleware = require('webpack-hot-middleware')(compiler, {
-  log: () => {}
+// webpack-hot-middleware的作用就是实现浏览器的无刷新更新
+const hotMiddleware = require('webpack-hot-middleware')(compiler, {
+  log: () => {},
 })
-//声明hotMiddleware无刷新更新的时机:html-webpack-plugin 的template更改之后
-compiler.plugin('compilation', function(compilation) {
-  compilation.plugin('html-webpack-plugin-after-emit', function(data, cb) {
+// 声明hotMiddleware无刷新更新的时机:html-webpack-plugin 的template更改之后
+compiler.plugin('compilation', (compilation) => {
+  compilation.plugin('html-webpack-plugin-after-emit', (data, cb) => {
     hotMiddleware.publish({
-      action: 'reload'
+      action: 'reload',
     })
     cb()
   })
 })
 // proxy api requests
-Object.keys(proxyTable).forEach(function(context) {
-  var options = proxyTable[context]
+Object.keys(proxyTable).forEach((context) => {
+  let options = proxyTable[context]
   if (typeof options === 'string') {
     options = {
-      target: options
+      target: options,
     }
   }
   app.use(proxyMiddleware(options.filter || context, options))
@@ -57,26 +58,26 @@ app.use(devMiddleware)
 // compilation error display
 app.use(hotMiddleware)
 // serve pure static assets
-var staticPath = path.posix.join(config.dev.assetsPublicPath, config.dev.assetsSubDirectory)
+const staticPath = path.posix.join(config.dev.assetsPublicPath, config.dev.assetsSubDirectory)
 app.use(staticPath, express.static('./static'))
-var uri = 'http://localhost:' + port
-var _resolve
-var readyPromise = new Promise(resolve => {
+const uri = `http://localhost:${port}`
+let _resolve
+const readyPromise = new Promise(resolve => {
   _resolve = resolve
 })
 console.log('> Starting dev server...')
 devMiddleware.waitUntilValid(() => {
-  console.log('> Listening at ' + uri + '\n')
+  console.log(`> Listening at ${uri}\n`)
   // when env is testing, don't need open it
   if (autoOpenBrowser && process.env.NODE_ENV !== 'testing') {
     opn(uri)
   }
   _resolve()
 })
-var server = app.listen(port)
+const server = app.listen(port)
 module.exports = {
   ready: readyPromise,
   close: () => {
     server.close()
-  }
+  },
 }
