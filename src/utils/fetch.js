8342ac3e738de8e5c20705fe14660be6575bf695
zhaoguohong <zhaoguohong@terminus.io>
2017-11-13 00:13:05 +0800

    remove  fetch polyfill

--- src/utils/fetch.js
+++ src/utils/fetch.js
@@ -1,7 +1,7 @@
 import { baseUrl } from '@/config'
 import { Toast } from 'mint-ui'
 
-export default async({ url = '', data = {}, type = 'GET', method = 'fetch' }) => {
+export default async({ url = '', data = {}, type = 'GET' }) => {
   type = type.toUpperCase()
   url = baseUrl + url
   if (type === 'GET') {
@@ -14,74 +14,43 @@ export default async({ url = '', data = {}, type = 'GET', method = 'fetch' }) =>
       url = `${url}?${dataStr}`
     }
   }
-  if (window.fetch && method === 'fetch') {
-    const requestConfig = {
-      credentials: 'include', // 传cookie
-      method: type,
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-      mode: 'cors',
-      cache: 'force-cache',
-    }
-    if (type === 'POST') {
-      Object.defineProperty(requestConfig, 'body', { value: JSON.stringify(data) })
-    }
-    try {
-      const response = await fetch(url, requestConfig)
-      const contentType = response.headers.get('content-type')
-      let responseData = ''
 
-      if (contentType && contentType.indexOf('application/json') !== -1) {
-        responseData = await response.json()
-      } else {
-        responseData = await response.text()
-      }
-      if ((response.status >= 200 && response.status <= 300) || response.status === 304) {
-        return responseData
-      }
-      Toast({
-        message: responseData,
-        duration: 1500,
-      })
-      return Promise.reject(responseData)
-    } catch (error) {
-      Toast({
-        message: error,
-        duration: 1500,
-      })
-      throw new Error(error)
+  const requestConfig = {
+    credentials: 'include', // 传cookie
+    method: type,
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    mode: 'cors',
+    cache: 'force-cache',
+  }
+  if (type === 'POST') {
+    Object.defineProperty(requestConfig, 'body', { value: JSON.stringify(data) })
+  }
+  try {
+    const response = await fetch(url, requestConfig)
+    const contentType = response.headers.get('content-type')
+    let responseData = ''
+
+    if (contentType && contentType.indexOf('application/json') !== -1) {
+      responseData = await response.json()
+    } else {
+      responseData = await response.text()
     }
-  } else {
-    return new Promise((resolve, reject) => {
-      let requestObj
-      if (window.XMLHttpRequest) {
-        requestObj = new XMLHttpRequest()
-      } else {
-        /*eslint-disable */
-        requestObj = new ActiveXObject
-      }
-      let sendData = ''
-      if (type === 'POST') {
-        sendData = JSON.stringify(data)
-      }
-      requestObj.open(type, url, true)
-      requestObj.setRequestHeader('Content-type', 'application/x-www-form-urlencoded')
-      requestObj.send(sendData)
-      requestObj.onreadystatechange = () => {
-        if (requestObj.readyState === 4) {
-          if (requestObj.status === 200) {
-            let obj = requestObj.response
-            if (typeof obj !== 'object') {
-              obj = JSON.parse(obj)
-            }
-            resolve(obj)
-          } else {
-            reject(requestObj)
-          }
-        }
-      }
+    if ((response.status >= 200 && response.status <= 300) || response.status === 304) {
+      return responseData
+    }
+    Toast({
+      message: responseData,
+      duration: 1500,
     })
+    return Promise.reject(responseData)
+  } catch (error) {
+    Toast({
+      message: error,
+      duration: 1500,
+    })
+    throw new Error(error)
   }
 }
