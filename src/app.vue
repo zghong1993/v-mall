e3e370922ccd4e01cc0a786dd328056b45f31b31
zhaoguohong <zhaoguohong@terminus.io>
2018-01-01 14:20:04 +0800

    bug fix

---
+++ src/app.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id='app'>
+    <transition name='slide-left' mode='out-in'>
+      <keep-alive>
+        <router-view v-if="$route.meta.keepAlive"></router-view>
+      </keep-alive>
+    </transition>
+    <transition name='slide-left' mode='out-in'>
+      <router-view v-if="!$route.meta.keepAlive"></router-view>
+    </transition>
+  </div>
+</template>
+<script>
+export default {
+  name: 'app',
+}
+
+</script>
+<style lang='less' scoped>
+.slide-left-enter-active {
+  left: 100%;
+  transition: left .5s;
+}
+
+.slide-left-enter-to {
+  left: 0;
+}
+
+</style>
